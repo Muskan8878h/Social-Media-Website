c8467717c0afb88501a2e2ebb1cc9e99a4c0dcea
muskan <muskan8878h@gmail.com>
2025-08-04 22:17:03 +0530

    we import all the file form pages folder in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
-
+import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import CreatePost from './pages/CreatePost'
+import Connections from './pages/Connections'
+import Profile from './pages/Profile'
+// import Login from './pages/Login'
 
 const App = ()=>{
   return (
-    <div>
-
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Login />}>
+         <Route index element={<Feed />}/>
+         <Route path='messages' element={<Messages />}/>
+         <Route path='messages/:userId' element={<ChatBox/>}/>
+         <Route path='connections' element={<Connections/>}/>
+         <Route path='discover' element={<Discover/>}/>
+         <Route path='profile' element={<Profile/>}/>
+         <Route path='profile/:profileId' element={<Profile/>}/>
+         <Route path='create-post' element={<CreatePost/>}/>
+        </Route>
+      </Routes>
+    </>
   )
 }
 
