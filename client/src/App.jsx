8898dc5865ad8d9ca98059da9096cb1a497a2e1e
muskan <muskan8878h@gmail.com>
2025-08-04 23:36:36 +0530

    content added in login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { Route, Routes } from "react-router-dom"
-import CreatePost from './pages/CreatePost'
-import Connections from './pages/Connections'
-import Profile from './pages/Profile'
-// import Login from './pages/Login'
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
+
+// import Login from './pages/Login';
+import CreatePost from './pages/CreatePost';
+import Connections from './pages/Connections';
+import Profile from './pages/Profile';
+import ChatBox from './pages/ChatBox';
+import Feed from './pages/Feed';
+import Discover from './pages/Discover';
+import Login from './pages/login';
+import Messages from './pages/Messages';
 
 const App = ()=>{
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />}>
+        <Route path='/' element={<Login />}/>
          <Route index element={<Feed />}/>
          <Route path='messages' element={<Messages />}/>
          <Route path='messages/:userId' element={<ChatBox/>}/>
@@ -18,7 +24,6 @@ const App = ()=>{
          <Route path='profile' element={<Profile/>}/>
          <Route path='profile/:profileId' element={<Profile/>}/>
          <Route path='create-post' element={<CreatePost/>}/>
-        </Route>
       </Routes>
     </>
   )
--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg'
 import sample_cover from './sample_cover.jpg'
-import sample_profile from './sampe_profile.jpg'
+import sample_profile from './sample_profile.jpg'
 import bgImage from './bgImage.png'
 import group_users from './group_users.png'
 import {Home, MessageCircle, Search, UserIcon, Users} from 'lucide-react'
-import sponsored_img from './sponsored_img.png'
+import sponsored_img from './sponsored_img.jpg'
 export const assets={
     logo,
     sample_cover,
