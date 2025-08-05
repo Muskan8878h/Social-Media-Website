f1c928e1a6818dcad9479530ebda8c4a28e480d9
muskan <muskan8878h@gmail.com>
2025-08-05 12:32:35 +0530

    we add sidebar  and  icones like profile , messages etc

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { assets } from '../assets/assets'
+import { Links, useNavigate } from 'react-router-dom'
+import MenuItems from './MenuItems'
+import { CirclePlus } from 'lucide-react'
+import {Link} from 'react-router-dom'
 
-const Sidebar = () => {
+
+const Sidebar = ({sidebarOpen, setSidebarOpen}) => {
+const navigate=useNavigate()
+
   return (
-    <div>
-      <h1>This is sidebar</h1>
+    <div className={`w-60 xl:w-72 bg-white border-r border-grey-200 flex flex-col justify-between items-center max-sm:absolute top-0 bottom-0 z-20 
+    ${sidebarOpen ? 'translate-x-0' : 'max-sm:-translate-x-full'} translate-all duration-300 ease-in-out `}>
+        <div className='w-full'>
+            <img onClick={()=> navigate('/') } src={assets.logo} className='w-26 ml-7 my-2 cursor-pointer' alt="" />
+            <hr className='border-grey-300 mb-8' />
+
+            <MenuItems setSidebarOpen={setSidebarOpen} />
+
+            <Link to='/create-post' className='flex items-center justify-center gap-2 py-2.5 mt-6 mx-6 rounded-lg bg-gradient-to-r from-indigo-500 to-purple-600 hover:from-indigo-700 hover:to-purple-800 active:scale-95 translation text-white cursor-pointer' >
+            <CirclePlus className='w-5 h-5' />
+            Create Post
+            </Link>
+            
+        </div>
     </div>
   )
 }
